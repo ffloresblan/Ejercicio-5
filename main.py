3cb804d8e549b729b0fc35b92b10ab3838d43956
ffloresblan <ffloresblanco@icloud.com>
2021-10-19 19:19:34 +0200

    ejercicio pycar

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 # Import a library of functions called 'pygame'
-import pygame
 from main_functions import *
 
 # Initialize the game engine
 pygame.init()
 
 #call main routine
+
+ancho = int(input("ancho de la ventana: "))
+alto = int(input("alto de la ventana: "))
 size = [1300, 600] # Define size of windows
-main2(size)
+size[0]=ancho
+size[1]=alto
+
+titulo = input("titulo: ")
+main2(size,titulo)
